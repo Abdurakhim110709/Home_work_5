f3a2b6bd51a19ea36d216702a14ca75b4ccb64b7
unknown <mkzrakhim@gmail.com>
2025-02-05 23:14:36 +0600

    First work

---
+++ movie_app/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Director, Movie, Review
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    director_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description', 'duration', 'director_name']  # Убираем "director", добавляем "director_name"
+
+    def get_director_name(self, obj):
+        return obj.director.name if obj.director else None
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
---
+++ movie_app/views.py
@@ -0,0 +1,33 @@
+from rest_framework import generics
+
+from .models import Director, Movie, Review
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+
+
+class DirectorListView(generics.ListCreateAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+class DirectorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+
+class MovieListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class ReviewListView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
